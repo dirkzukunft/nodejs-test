4e000a5282f4a9c49271d574e8471f5fe8fbd04d
dirkzukunft <git@qf3.de>
2021-08-15 00:45:42 +0200

    Add POST request and sample.http

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,16 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Hello, the server is running :)');
+app.get('/', (req, res) => {
+  res.send(`Hello, the server is running and data is ${JSON.stringify(req.query)}`);
+});
+
+app.get('/test', (req, res) => {
+  res.send(`Oh, a test! :) ... and more data? ${JSON.stringify(req.query)}`);
+});
+
+app.post('/', (req, res) => {
+  res.send(`Oh yes, I got a POST request :) And the data is: ${JSON.stringify(req.body)}`);
 });
 
 app.listen(port, async () => {
