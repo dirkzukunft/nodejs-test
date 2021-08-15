8a3ebab20cd1b4f6e6f3bad41f2108fc81a56ce2
dirkzukunft <git@qf3.de>
2021-08-15 12:37:35 +0200

    Install helmet

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const port = 3000;
 const app = express();
 app.use(express.json());
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send(`Hello, the server is running and data is ${JSON.stringify(req.query)}`);
