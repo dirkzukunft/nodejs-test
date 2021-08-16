65666225a13ed223735f22bf5cc27c758b90170e
dirkzukunft <git@qf3.de>
2021-08-16 12:26:04 +0200

    change import order to convention

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import helmet from 'helmet';
 import dotenv from 'dotenv';
 dotenv.config();
+import express from 'express';
+import helmet from 'helmet';
 
 const port = 3000;
 const app = express();
